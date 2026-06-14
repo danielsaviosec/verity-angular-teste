3acc47d6e319c6b641e2e1287837287f334b1f1d
Daniel Secundino <danielsecundino@MacBook-Pro-de-Daniel.local>
2026-06-14 16:41:38 -0300

    chore: adicionado jest, husky e lint ao projeto

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/src/**/*.spec.ts'],
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!src/app/**/*.spec.ts',
+    '!src/app/**/*.module.ts',
+    '!src/app/**/*.config.ts',
+    '!src/app/**/*.config.server.ts',
+    '!src/app/**/*.routes.ts',
+    '!src/app/**/*.routes.server.ts',
+    '!src/app/**/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  coverageReporters: ['text', 'lcov', 'html'],
+};
+
+export default config;
